e351193a812037e4464904f727342f521f7ea150
Freddy Calk <freddycalk@gmail.com>
2015-12-14 22:36:31 -0500

    lots of inclusions
    made and styled the login/signup pages, began styling the home page.
    set up get and post routers for the login and registration pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
+var passport = require('passport');
+var Account = require('../models/accounts');
 var router = express.Router();
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express', content: 'Stupid' });
 });
 
+
+router.get('/register', function (req, res, next){
+	res.render('register',{ title: 'Express', content: 'Stupid' })
+})
+
+router.post('/register', function (req, res, next){
+	Account.register(new Account(
+		{username: req.body.username}),
+		req.body.password,
+		function (err, account){
+			if(err){
+				console.log(err)
+				return res.render('index')
+			}else{
+				passport.authenticate('local') (req, res, function (){
+					res.redirect('/');
+				})
+			}
+		}
+	)
+})
+
+router.get('/login', function (req, res, next){
+	res.render('login')
+})
+
+router.post('/login', function (req, res, next){
+	res.render('login')
+})
 module.exports = router;
